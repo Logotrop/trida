09f2432e7854a9372273d613ad348b3f0e90e9f8
Logotrop <keramdobes@gmail.com>
2016-12-12 00:43:28 +0100

    update

--- 3.ročník/Batch/rozdelovac.bat
+++ 3.ročník/Batch/rozdelovac.bat
@@ -2,9 +2,22 @@
 title rozdělovač
 mode con: cols=80 lines=20
 set /p wordlist=
-for /f "tokens=*" %%i in (%wordlist%.txt) do (
-echo : >> %wordlist%.txt
-)
+set /a a=2
+set /a b=10
+set first6=%wordlist:~0,b% 
+echo %first6%
+pause
+:repeat
+set first8=%wordlist:~%%a,%%b%:
+set wordlist:~%%a,%%b=%first8%
+echo %first8%
+pause
+set /a a=%a%+8
+set /a b=%b%+8
+if %wordlist:~%%a+1% != "" (goto repeat)
+pause
+
+
 echo zkontrolovat soubor? (y/n) :
 set /p file=
 if file=="y" (
